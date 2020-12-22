c6c5f346ee5218e919f540508092b452a862bef9
Sasha <lonagi22@gmail.com>
2020-12-22 22:03:57 +0200

    sprite

--- src/Car.py
+++ src/Car.py
@@ -3,8 +3,6 @@ import pygame
 
 class Car:
     def __init__(self):
-        self.random_sprite()
-
         self.angle = 0
         self.speed = 5
         self.radars = []
@@ -13,3 +11,16 @@ class Car:
         self.goal = False
         self.distance = 0
         self.time_spent = 0
+        self.car_sprite = ""
+        self.car = ""
+
+        self.random_sprite()
+
+    def random_sprite(self):
+        self.car_sprite = pygame.image.load('../res/car.png')
+        self.car_sprite = pygame.transform.scale(self.car_sprite,
+                                                 (math.floor(self.car_sprite.get_size()[0] / 2),
+                                                  math.floor(self.car_sprite.get_size()[1] / 2)))
+        self.car = self.car_sprite
+        self.pos = [650, 930]
+        self.find_pivot()
