3cf83766dd9a2edcb40faaee8697aab8bdc5331b
Sasha <lonagi22@gmail.com>
2020-12-22 22:14:09 +0200

    calculations

--- src/Car.py
+++ src/Car.py
@@ -42,6 +42,21 @@ class Car:
 
     def draw_center(self, screen):
         pygame.draw.circle(screen, (0, 72, 186), (math.floor(self.center[0]), math.floor(self.center[1])), 5)
+
+    def compute_radars(self, degree, road):
+        length = 0
+        x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
+        y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * length)
+
+
+        while not road.get_at((x, y)) == bg and length < 300:
+            length = length + 1
+            x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
+            y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * length)
+
+        dist = int(math.sqrt(math.pow(x - self.center[0], 2) + math.pow(y - self.center[1], 2)))
+        self.radars.append([(x, y), dist])
+
     def compute_collision_points(self):
         self.find_pivot()
         lw = 65
@@ -57,3 +72,14 @@ class Car:
               self.center[1] + math.sin(math.radians(360 - (self.angle + 340))) * lh]
 
         self.collision_points = [lt, rt, lb, rb]
+    def check_collision(self, road):
+        self.is_alive = True
+        for p in self.collision_points:
+            try:
+                if road.get_at((int(p[0]), int(p[1]))) == bg:
+                    self.is_alive = False
+                    break
+            except IndexError:
+                self.is_alive = False
+    def get_reward(self):
+        return self.distance / 50.0
