97a7fd85f81267617d4cb2a687f37663aadb6db1
Sasha <lonagi22@gmail.com>
2021-01-02 22:48:24 +0200

    Car

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ height=1100
 bg=(213,193,154,255)
 
 generation=0
+
+class Car:
+	def __init__(self):
+		self.random_sprite()
+
+		self.angle=0
+		self.speed=5
+
+		self.radars=[]
+		self.collision_points=[]
+
+		self.is_alive=True
+		self.goal=False
+		self.distance=0
+		self.time_spent=0
